ed6ce19375351c9943fbb444ba49d3c4f761f4be
Kalatyn11 <Kalatyn11@gmail.com>
2017-02-03 23:20:20 +0300

    Проект

---
+++ Gta5Project/GtaClient/Helper/Debug/ConsoleWriter.cs
@@ -0,0 +1,43 @@
+﻿using GTA;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GtaClient.Helper.Debug
+{
+
+    public class ConsoleWriter : Script
+    {
+        Ped ped;
+        public ConsoleWriter()
+        {
+            Tick += OnTick;
+            Interval = 100;
+            ped = GTA.World.CreateRandomPed(Game.Player.Character.Position.Around(1));
+#if DEBUG
+            DebugExtention.ShowConsoleWindow();
+#endif
+        }
+
+        readonly bool[] _active = new bool[2];
+        readonly DateTime[] _timeLeft = new DateTime[2];
+
+        void OnTick(object sender, EventArgs e)
+        {
+            Console.Clear();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            if (!ped.IsRunning && !ped.IsWalking)
+            {
+                ped.Task.GoTo(Game.Player.Character.Position);
+            }
+
+            watch.Stop();
+
+            Console.WriteLine($"ElapsedMilliseconds:{watch.ElapsedMilliseconds}");
+        }
+    }
+}
---
+++ Gta5Project/GtaLauncher/Game.cs
@@ -0,0 +1,55 @@
+﻿using AssemblyLoader;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GtaLauncher
+{
+    public class Game:IDisposable
+    {
+        Process gtaProcess;
+
+        AppDomain GtaDomain;
+
+        public string PathToExe = @"C:\Games\Grand Theft Auto V\GTAVLauncher.exe";
+
+        public string AssemblyPath = $@"{AppDomain.CurrentDomain.BaseDirectory}libs\";
+
+        public Game()
+        {
+            GtaDomain = AppDomain.CreateDomain("GTA");
+        }
+
+        public void Start()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(PathToExe);
+            startInfo.CreateNoWindow = false;
+            startInfo.WorkingDirectory = Path.GetDirectoryName(PathToExe);
+            startInfo.Arguments = "-scOfflineOnly";
+            startInfo.Domain = "GTA";
+            gtaProcess = Process.Start(startInfo);
+            LoadAssembly();
+        }
+
+        public void LoadAssembly()
+        {
+            Type typeName = typeof(Loader);
+            Loader loader = (Loader)GtaDomain.CreateInstanceFromAndUnwrap($"{AppDomain.CurrentDomain.BaseDirectory}AssemblyLoader.dll", typeName.FullName);
+            loader.ShowConsole();
+            loader.LoadNative($@"{AssemblyPath}\ScriptHookV.dll");
+            loader.LoadNative($@"{AssemblyPath}\ScriptHookVDotNet.dll");
+            
+        }
+
+        public void Dispose()
+        {
+            gtaProcess.Kill();
+        }
+    }
+}
